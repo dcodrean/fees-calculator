e2ec600eee391fb6b7e458469b6c2bacde6d46a4
dcodrean <dan.codrean@gmasoftware.com>
2020-05-13 14:55:49 +0300

    #refactor FeeCalculator

--- src/main/java/compute/FeeCalculator.java
+++ src/main/java/compute/FeeCalculator.java
@@ -5,7 +5,10 @@ import model.entities.FeeApplicationResult;
 import model.entities.FeeRule;
 import providers.AccountProvider;
 import providers.FeeRulesProvider;
+import providers.IAccountProvider;
+import providers.IFeeRulesProvider;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,20 +17,44 @@ import java.util.List;
 public class FeeCalculator {
     List<Account> listAccounts;
 
+    IAccountProvider accountProvider;
+    IFeeRulesProvider feeRulesProvider;
+
+    /**
+     * @param accountProvider
+     * @param feeRulesProvider
+     */
+    public FeeCalculator(IAccountProvider accountProvider, IFeeRulesProvider feeRulesProvider) {
+        this.accountProvider = accountProvider;
+        this.feeRulesProvider = feeRulesProvider;
+    }
+
     /**
      * @param feeCalculationRequest
      * @return
      */
     private List<FeeApplicationResult> getFeePerTrade(FeeCalculationRequest feeCalculationRequest) {
 
-        List<FeeRule> feeRules = new FeeRulesProvider().getAll();
-        Account account = new AccountProvider().get(feeCalculationRequest.getAccountId());
+        List<FeeRule> feeRules = feeRulesProvider.getAll();
+        Account account = accountProvider.get(feeCalculationRequest.getAccountId());
 
         for (FeeRule feeRule : feeRules) {
 
         }
-        return null;
+        return new ArrayList<>();
     }
 
+    /**
+     * TO BE REMOVED
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        IAccountProvider accountProvider = new AccountProvider();
+        IFeeRulesProvider feeRulesProvider = new FeeRulesProvider();
+
+        FeeCalculator feeCalculator = new FeeCalculator(accountProvider, feeRulesProvider);
+        feeCalculator.getFeePerTrade(new FeeCalculationRequest());
+    }
 
 }
